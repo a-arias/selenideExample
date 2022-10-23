fef48520113a8429c56755842f9657476c83fcb7
Abram Arias <you@example.com>
2022-10-23 13:28:55 -0700

    removes some spaces on imports

--- src/test/java/com/amazon/example/pageObjects/cartPage.java
+++ src/test/java/com/amazon/example/pageObjects/cartPage.java
@@ -1,5 +1,4 @@
 package com.amazon.example.pageObjects;
-
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 import static com.codeborne.selenide.Condition.visible;
--- src/test/java/com/amazon/example/pageObjects/cartSideBarPage.java
+++ src/test/java/com/amazon/example/pageObjects/cartSideBarPage.java
@@ -1,8 +1,6 @@
 package com.amazon.example.pageObjects;
-
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
-
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
