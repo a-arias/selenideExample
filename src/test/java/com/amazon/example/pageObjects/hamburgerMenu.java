2b7daa2de3a4750838a9811a2246e9b48257bcab
Abram Arias <you@example.com>
2022-10-23 13:24:24 -0700

    adds documentation on hamburgerMenu

--- src/test/java/com/amazon/example/pageObjects/hamburgerMenu.java
+++ src/test/java/com/amazon/example/pageObjects/hamburgerMenu.java
@@ -1,7 +1,4 @@
 package com.amazon.example.pageObjects;
-
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
@@ -9,28 +6,32 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class hamburgerMenu extends basePage {
-    //Locators
+    //###Locators
     public SelenideElement HamburgerMenu() {return $(By.id("nav-hamburger-menu"));}
     public SelenideElement MenuLink(String name) {return $(By.xpath("//a[@data-menu-id]/div[text()="+"'"+name+"'"+"]"));}
     public SelenideElement SubMenuLink(String element) { return $(By.xpath("//a[@class='hmenu-item'][(text()="+"'"+element+"'"+")]"));}
     public SelenideElement checkboxButton(String name) {return $(By.xpath("//a/span[(text()="+"'"+name+"'"+")]"));}
 
-    //Complex methods
+    //###class methods
+    // opens up hamburger menu.
     public hamburgerMenu openHamburgerMenu() {
         HamburgerMenu().shouldBe(visible).click();
         return this;
     }
 
+    //clicks menu link inside hamburger menu.
     public hamburgerMenu clickMenuLink(String name) {
         MenuLink(name).shouldBe(visible).click();
         return this;
     }
 
+    //clicks sub menu item inside hamburger menu.
     public hamburgerMenu clickSubsectionMenuLink(String name) {
         SubMenuLink(name).shouldBe(visible).click();
         return this;
     }
 
+    //clicks checkbox from hamburger menu.
     public void clickCheckboxButton(String name) {
         checkboxButton(name).shouldBe(visible).scrollIntoView(true).click();
     }
