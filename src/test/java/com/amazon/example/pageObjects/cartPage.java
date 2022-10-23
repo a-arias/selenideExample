0e1234325bdb72dbf2999735591d7e255966f2d6
Abram Arias <you@example.com>
2022-10-23 13:21:11 -0700

    adds documentation on cartPage

--- src/test/java/com/amazon/example/pageObjects/cartPage.java
+++ src/test/java/com/amazon/example/pageObjects/cartPage.java
@@ -7,18 +7,13 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class cartPage extends basePage {
-    public SelenideElement resultsElement(String index) {
-        return $(By.cssSelector("[data-item-index="+"'"+index+"'"+"]"));
-    }
+    //#########Locators
+    public SelenideElement resultsElement(String index) {return $(By.cssSelector("[data-item-index="+"'"+index+"'"+"]"));}
 
+    //#########Class methods
+    //Clicks product from list
     public cartPage clickProductFromList(String index) {
         resultsElement(index).shouldBe(visible).click();
         return this;
     }
-
-//    public cartPage clickProductFromList(String index) {
-//         $(By.xpath("//span[@class='a-size-medium a-color-base a-text-normal'][index]"));
-//         return this;
-//
-//    }
 }
