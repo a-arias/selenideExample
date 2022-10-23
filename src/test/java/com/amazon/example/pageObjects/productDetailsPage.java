48cd857b21cf2b3de9989ca8bed32ee2620f84cd
Abram Arias <you@example.com>
2022-10-23 13:10:12 -0700

    adds documentation on productDetailsPage

--- src/test/java/com/amazon/example/pageObjects/productDetailsPage.java
+++ src/test/java/com/amazon/example/pageObjects/productDetailsPage.java
@@ -9,38 +9,34 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class productDetailsPage extends basePage {
-    //Locators
-    public SelenideElement aboutThisItemHeader() {
-        return Selenide.$(By.xpath("//h1[contains(text(),'About this item')]"));
-    }
+    //#Locators
+    public SelenideElement aboutThisItemHeader() {return Selenide.$(By.xpath("//h1[contains(text(),'About this item')]"));}
     public SelenideElement productTitle() {
         return $(By.id("productTitle"));
     }
-    public SelenideElement addToCartButton() {
-        return $(By.id("add-to-cart-button"));
-    }
+    public SelenideElement addToCartButton() {return $(By.id("add-to-cart-button"));}
     public SelenideElement buyNowButton() {
         return $(By.id("buy-now-button"));
     }
-
-    public SelenideElement addToWishListButton() {
-        return $(By.id("wishListMainButton"));
-    }
-
+    public SelenideElement addToWishListButton() {return $(By.id("wishListMainButton"));}
     public SelenideElement ProductTitle() {
         return $(By.id("productTitle"));
     }
 
+    //###class methods
+    //Clicks the add to wish list button on product details page.
     public productDetailsPage clickAddToWishListButton() {
         addToWishListButton().shouldBe(visible).click();
         return this;
     }
 
+    //Clicks the add to cart button on product details page.
     public productDetailsPage clickAddToCartButton() {
         addToCartButton().shouldBe(visible).click();
         return this;
     }
 
+    //get current product title.
     public String getCurrentProductTitle() {
         return ProductTitle().getText();
     }
