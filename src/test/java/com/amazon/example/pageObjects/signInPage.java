df51660ba907423131b5b3ca7e14ef393d18f56b
Abram Arias <you@example.com>
2022-10-23 13:27:16 -0700

    adds documentation on signInPage

--- src/test/java/com/amazon/example/pageObjects/signInPage.java
+++ src/test/java/com/amazon/example/pageObjects/signInPage.java
@@ -1,13 +1,12 @@
 package com.amazon.example.pageObjects;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
-
 import static com.codeborne.selenide.Selenide.$;
 
 public class signInPage extends basePage {
+    //#Locators
     public SelenideElement signInHeader() {
         return $(By.cssSelector("h1"));
     }
 
 }
-
