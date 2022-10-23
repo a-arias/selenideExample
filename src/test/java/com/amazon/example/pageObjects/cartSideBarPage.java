7664c12669c57af9414cde57cfa4ebec94562e0e
Abram Arias <you@example.com>
2022-10-23 13:12:35 -0700

    adds documentation on cartSideBarPage

--- src/test/java/com/amazon/example/pageObjects/cartSideBarPage.java
+++ src/test/java/com/amazon/example/pageObjects/cartSideBarPage.java
@@ -7,15 +7,12 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class cartSideBarPage extends basePage {
+    //###Locators
+    public SelenideElement goToCartButton() {return $(By.cssSelector("input[type='submit']"));}
+    public SelenideElement proceedToCheckoutButton() {return $(By.id("attach-sidesheet-checkout-button-announce"));}
 
-    public SelenideElement goToCartButton() {
-        return $(By.cssSelector("input[type='submit']"));
-    }
-
-    public SelenideElement proceedToCheckoutButton() {
-        return $(By.id("attach-sidesheet-checkout-button-announce"));
-    }
-
+    //###class methods
+    //clicks go to cart button inside the cart menu component.
     public cartSideBarPage clickGoToCartButton() {
         goToCartButton().shouldBe(visible).click();
         return this;
