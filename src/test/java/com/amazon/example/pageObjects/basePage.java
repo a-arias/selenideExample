b5fd15a78af8a78275edac1e87a9715c33b0cd20
Abram Arias <you@example.com>
2022-10-22 20:29:42 -0700

    adds basePage

---
+++ src/test/java/com/amazon/example/pageObjects/basePage.java
@@ -0,0 +1,12 @@
+package com.amazon.example.pageObjects;
+
+import com.codeborne.selenide.Selenide;
+
+public class basePage {
+
+    public basePage switchToNewWindow() {
+        Selenide.switchTo().window(1);
+        return this;
+    }
+
+}
